7959b715c9b05a425fe7a021c1828a654714270c
sacnakag <sacnakag@yahoo-corp.jp>
2019-01-02 02:22:19 +0900

    commit

---
+++ src/main/java/com/github/sasachichito/cd_webapi/application/presentationmodel/UserPresentationModel.java
@@ -0,0 +1,19 @@
+package com.github.sasachichito.cd_webapi.application.presentationmodel;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.github.sasachichito.cd_webapi.domain.model.user.User;
+
+public class UserPresentationModel {
+
+    @JsonValue
+    private String userName;
+
+    public UserPresentationModel(User user) {
+        this.initializeFrom(user);
+    }
+
+    private void initializeFrom(User user) {
+        this.userName = user.name().name();
+    }
+
+}
--- src/main/java/com/github/sasachichito/cd_webapi/port/adapter/web/UserController.java
+++ src/main/java/com/github/sasachichito/cd_webapi/port/adapter/web/UserController.java
@@ -1,6 +1,7 @@
 package com.github.sasachichito.cd_webapi.port.adapter.web;
 
 import com.github.sasachichito.cd_webapi.application.UserService;
+import com.github.sasachichito.cd_webapi.application.presentationmodel.UserPresentationModel;
 import com.github.sasachichito.cd_webapi.domain.model.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,9 +23,9 @@ public class UserController {
     }
 
     @RequestMapping(method= RequestMethod.GET, value="{name}")
-    public User ofName(@PathVariable String name) {
-        // TODO convert
-        return this.userService.ofName(name);
+    public UserPresentationModel ofName(@PathVariable String name) {
+        User user = this.userService.ofName(name);
+        return new UserPresentationModel(user);
     }
 
     @RequestMapping(method= RequestMethod.GET)
