13f196d2990a565a4d652b0ccce585406e49a8fe
sacnakag <sacnakag@yahoo-corp.jp>
2019-01-02 02:57:22 +0900

    commit

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# init
+# DB構築
 ```
 $ docker run --name mysql -e MYSQL_ROOT_PASSWORD=mysql -d -p 3306:3306 mysql
 $ mysql -u root -p -h 127.0.0.1 -e 'create database if not exists my_db;'
+```
+
+# App起動
+```
+$ ./gradlew build
+$ java -Dspring.profiles.active=dev -jar build/libs/cd_webapi-0.0.1-SNAPSHOT.jar
 ```
--- src/test/java/com/github/sasachichito/cd_webapi/CdWebapiApplicationTests.java
+++ src/test/java/com/github/sasachichito/cd_webapi/CdWebapiApplicationTests.java
@@ -1,16 +1,12 @@
 package com.github.sasachichito.cd_webapi;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class CdWebapiApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
